71787be7d5e82ff858362e22b268a8748302f67a
Tomáš Horáček <tomas.horacek@gmail.com>
2012-10-23 14:49:36 +0200

    ling from admin to booking overview and back

--- urls.py
+++ urls.py
@@ -3,6 +3,8 @@ from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+admin.site.index_template = 'admin_index.html'
+
 urlpatterns = patterns('',
     url(r'^rezervace/', include('booking.urls')),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
